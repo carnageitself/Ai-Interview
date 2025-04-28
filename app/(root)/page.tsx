4e3d8c9f881d80c277b6616123c927dca70e96a3
Vikramop <veervikramsingh132@gmail.com>
2025-04-28 23:52:59 +0530

    previous interviews display

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,33 @@
 import InterviewCard from '@/components/InterviewCard';
 import { Button } from '@/components/ui/button';
-import { dummyInterviews } from '@/constants';
+import {
+  getCurrentUser,
+  getInterveiwsByUserId,
+  getLatestInterviews,
+} from '@/lib/actions/auth.action';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-const page = () => {
+const Page = async () => {
+  const user = await getCurrentUser();
+
+  console.log('user', user?.id);
+  if (user?.id === undefined) {
+    throw new Error('User ID is undefined');
+  }
+
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterveiwsByUserId(user?.id),
+    await getLatestInterviews({ userId: user?.id }),
+  ]);
+
+  // const userInterviews = await getInterveiwsByUserId(user?.id!);
+  // const getLatestInterviews = await getLatestInterviews({user?.id!});
+  console.log('userInterviews', userInterviews);
+  const hasPastInterviews = userInterviews?.length > 0;
+  const hasUpcomingInterviews = latestInterviews?.length > 0;
+
   return (
     <>
       <section className="card-cta">
@@ -30,23 +52,29 @@ const page = () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-          {/* <p>You havn&apos;t taken any interviews yet</p> */}
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
+          {hasPastInterviews ? (
+            userInterviews.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>You havn&apos;t taken any interviews yet</p>
+          )}
         </div>
       </section>
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take an interview</h2>
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
-          {/* <p>There are no interviews available</p> */}
+          {hasUpcomingInterviews ? (
+            latestInterviews.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>There are no new interviews available</p>
+          )}
         </div>
       </section>
     </>
   );
 };
 
-export default page;
+export default Page;
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -119,3 +119,37 @@ export async function isAuthenticated() {
   return !!user;
   //!! to convert a truthy or falsy value to a boolean value
 }
+
+export async function getInterveiwsByUserId(
+  userId: string
+): Promise<Interview[] | null> {
+  const interviews = await db
+    .collection('interviews')
+    .where('userId', '==', userId)
+    .orderBy('createdAt', 'desc')
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+}
+
+export async function getLatestInterviews(
+  params: GetLatestInterviewsParams
+): Promise<Interview[] | null> {
+  const { userId, limit = 20 } = params;
+
+  const interviews = await db
+    .collection('interviews')
+    .orderBy('createAt', 'desc')
+    .where('finalized', '==', true)
+    .where('userId', '!=', userId)
+    .limit(limit)
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+}
